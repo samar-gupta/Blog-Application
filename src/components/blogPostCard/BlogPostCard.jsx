984aa545c5ffe594e7d0e17807556ed2db745276
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-08-07 22:42:34 +0530

    Update BlogPostCard.jsx

--- src/components/blogPostCard/BlogPostCard.jsx
+++ src/components/blogPostCard/BlogPostCard.jsx
@@ -73,7 +73,7 @@ function BlogPostCard() {
                             ? 'rgb(226, 232, 240)'
                             : ' rgb(30, 41, 59)'
                         }}>
-                          Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.
+                          Click banner to know more about this topic.
                         </p>
                       </div>
                     </div>
