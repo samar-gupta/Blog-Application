d0c79047b46e1c9f2e960beec13f8a30d27646b0
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-08-07 22:44:49 +0530

    Update AllBlogs.jsx

--- src/pages/allBlogs/AllBlogs.jsx
+++ src/pages/allBlogs/AllBlogs.jsx
@@ -85,7 +85,7 @@ function AllBlogs() {
                                                             ? 'rgb(226, 232, 240)'
                                                             : ' rgb(30, 41, 59)'
                                                     }}>
-                                                        Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.
+                                                        Click banner to know more about this topic.
                                                     </p>
                                                 </div>
                                             </div>
